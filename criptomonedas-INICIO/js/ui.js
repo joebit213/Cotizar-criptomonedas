8faa8f248b5b403ea045f16a986a1d2628643745
joseph <josephfguillen@gmail.com>
2020-02-05 12:01:02 -0600

    finish :octocat:

--- criptomonedas-INICIO/js/ui.js
+++ criptomonedas-INICIO/js/ui.js
@@ -14,7 +14,7 @@ class Interfaz {
         //crear un selec de opciones -- toma el elemento 
         const select = document.querySelector('#criptomoneda')
 
-        //iteerar por los resultados de la api
+        //iterar por los resultados de la api
         //llave y valor del objeto lo recorres con of 
         //donde key es el nombre de la criptomoneda
         //value son sus datos
@@ -26,7 +26,7 @@ class Interfaz {
           select.appendChild(opcion)
         }
 
-        //toma el objeto y lo convierte a un array -- no rrecorre todos los objetos
+        //toma el objeto y lo convierte a un array -- no recorre todos los objetos
         //toma la llave y su valor y los retorna en un arreglo
         //console.log(Object.entries(monedas.monedas.Data))
       })
@@ -48,6 +48,52 @@ class Interfaz {
 
   //Imprime el resultado de la cotizacion
   mostrarResultado(resultado, moneda, criptomoneda) {
-    console.log(resultado[criptomoneda])
+
+    //en caso de un resultado anterior, ocultarlo
+    const resultadoAnterior = document.querySelector('#resultado > div')
+    if(resultadoAnterior) {
+      resultadoAnterior.remove()
+    } 
+
+    console.log(resultado[criptomoneda][moneda])
+   const datosMoneda = resultado[criptomoneda][moneda]
+
+   //recortar digitos de precio
+   let precio = datosMoneda.PRICE.toFixed(2)
+   let porcentaje = datosMoneda.CHANGEPCTDAY.toFixed(2)
+   let actualizado = new Date(datosMoneda.LASTUPDATE * 1000).toLocaleDateString('es-MX')
+
+    //construir el template
+    let templateHTML = `
+              <div class="card bg-warning">
+                <div class="card-body text-light">
+                  <h2 class="card-title">Resultado:</h2>
+                  <p>El precio de ${datosMoneda.FROMSYMBOL} a moneda ${datosMoneda.TOSYMBOL}
+                      es de: $ ${precio}    
+                  </p>
+                  <p>Variacion del ultimo dia: % ${porcentaje}</p>
+                  <p>Ultima actualizacion: ${actualizado}</p>
+                </div>
+              </div> 
+    `
+
+    //insertar spinner
+    this.mostrarOcultarSpinner('block')
+    
+    //insertar el resultado
+    setTimeout(() => {
+      document.querySelector('#resultado').innerHTML = templateHTML
+
+      //ocultar spinner
+      this.mostrarOcultarSpinner('none')
+    },2000)
+
   }
+
+  //Mostrar un spinner de carga al pedir la cotizacion
+  mostrarOcultarSpinner(vista) {
+    const spinner = document.querySelector('.contenido-spinner')
+    spinner.style.display = vista
+  }
+
 }
